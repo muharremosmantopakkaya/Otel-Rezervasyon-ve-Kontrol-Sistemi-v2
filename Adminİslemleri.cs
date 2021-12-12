2ff0f426056a3c68adc2f53752498f693efe5f82
muharremosmantopakkaya <canbejing20@hotmail.com>
2021-12-12 15:16:23 +0300

    OTEL REZERVASYON ve KONTROL SİSTEMİ
    Ara sınav için oluşturduğum dosyalar

---
+++ Adminİslemleri.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace urunotomasyon
+{
+    public partial class Adminİslemleri : Form
+    {
+        public Adminİslemleri()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            FrmAdmin fr = new FrmAdmin();
+            fr.Show();
+            this.Hide(); //gizlemek
+        }
+
+        private void bunifuThinButton21_Click(object sender, EventArgs e)
+        {
+            GelirGider fr = new GelirGider();
+
+            fr.Show();
+            this.Hide(); //gizlemek
+        }
+
+        private void bunifuThinButton22_Click(object sender, EventArgs e)
+        {
+            StokFrm fr = new StokFrm();
+            fr.Show();
+            this.Hide(); //gizlemek
+        }
+
+        private void Adminİslemleri_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SifreBtn_Click(object sender, EventArgs e)
+        {
+            SifreGuncellemeIslemleri fr = new SifreGuncellemeIslemleri();
+            fr.Show();
+            this.Hide();
+        }
+
+        private void FeedBackMus_Click(object sender, EventArgs e)
+        {
+            GeriBildirimMusteri fr = new GeriBildirimMusteri();
+            fr.Show();
+            this.Hide();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+namespace urunotomasyon
+{
+    static class Program
+    {
+        /// <summary>
+        /// Uygulamanın ana girdi noktası.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(true);
+            Application.Run(new AnaSayfa());
+        }
+    }
+}
